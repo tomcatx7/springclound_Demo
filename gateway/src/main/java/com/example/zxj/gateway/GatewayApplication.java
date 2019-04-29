630a59cabbd50a872802e0f3ab4aca65908af267
zhongxj <zhongxj@chinawiserv.com>
2019-04-29 10:18:27 +0800

    add zipkin

--- gateway/src/main/java/com/example/zxj/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/example/zxj/gateway/GatewayApplication.java
@@ -14,6 +14,5 @@ public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
-
 }
 
---
+++ zipkin/src/main/java/com/example/zxj/zipkin/ZipkinApplication.java
@@ -0,0 +1,17 @@
+package com.example.zxj.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zipkin2.server.internal.EnableZipkinServer;
+
+@SpringBootApplication
+// client add the dependency of spring-cloud-starter-zipkin
+// and  SpringBootApplication could autoconfigure and register to the zipkinserver
+@EnableZipkinServer
+public class ZipkinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinApplication.class, args);
+    }
+
+}
